650d572456b09058a30e7499ba9ba31fc503c0cd
Jonathon Alsum <jneala@gmail.com>
2022-04-28 18:00:06 -0500

    finished generateParentheses for leetCode using recursion

--- Recursive/generateParenthesis.js
+++ Recursive/generateParenthesis.js
@@ -13,20 +13,25 @@ var generateParenthesis = function(n) {
   return result;
 };
 
-//Recursive function, taking in the resulting array, a string s, a count for open, a count for closed, and the given n.
+//Recursive function, taking in the resulting array, a string s, a count for open, a count for close, and the given n.
 function generate(result, s, open, close, n){
   //If the open and close parentheses are both equal to n, push the string to result.
   if(open === n && close === n){
     result.push(s);
+    console.log(s)
     return;
   }
   //If open is less than n, call the recursive function, add an open parentheses, add 1 to open count, and pass the other variables.
   if(open < n){
+    console.log("hits open inside: " + s)
     generate(result, s + "(", open + 1, close, n);
   }
-  if(close < n){
+  console.log("hits open outside")
+  if(close < open){
+    console.log("hits close inside: " + s)
     generate(result, s + ")", open, close + 1, n);
   }
+  console.log("hits close outside")
 };
 
 let n = 3;
