aafa973cda0b4ef8b9e1c93431924ffab2a3bcc4
Jonathon Alsum <jneala@gmail.com>
2022-05-11 14:00:40 -0500

    adding maxDepthBinaryTree in Python, recursive DFS, iterative DFS, and iterative BFS

--- Python Algos/maxDepthBinaryTree.py
+++ Python Algos/maxDepthBinaryTree.py
@@ -1,4 +1,5 @@
 # Definition for a binary tree node.
+from collections import deque
 from typing import Optional
 
 
@@ -18,8 +19,32 @@ class Solution:
         
     # DFS
     def maxDepthIterative(self, root: Optional[TreeNode]) -> int:
-        pass
+        stack = [[root, 1]]
+        res = 0
+        
+        while stack:
+            node, depth = stack.pop()
+            
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
     
-    # Breath FS
-    def maxDepthRecursive(self, root: Optional[TreeNode]) -> int:
-        pass
+    # Breath FS, not a lot of benifits, but educational
+    def maxDepthIterative(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        level = 0
+        q = deque([root])
+        
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                    
+            level += 1
+        return level
